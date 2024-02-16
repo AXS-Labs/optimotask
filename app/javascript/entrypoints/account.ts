9268dddc772d94b8d10c14f6aeb93c6e4de19492
Alexander Schrot <alexander@axs-labs.com>
2024-02-16 17:29:54 +0100

    setup stimulus with vite

--- app/javascript/entrypoints/account.ts
+++ app/javascript/entrypoints/account.ts
@@ -1,1 +1,9 @@
+import "@hotwired/turbo-rails"
 import "@tabler/core"
+import { Application } from "@hotwired/stimulus"
+import { definitionsFromGlob } from 'stimulus-vite-helpers'
+
+const controllers = import.meta.glob('../account/controllers/**/*_controller.(js|ts)', { eager: true })
+
+const application = Application.start()
+application.load(definitionsFromGlob(controllers))
