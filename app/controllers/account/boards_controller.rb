521a62d1f5de7e6ac570fdbc35a4663bee3b45bb
Alexander Schrot <alexander@axs-labs.com>
2024-02-17 14:25:21 +0100

    add new form for boards

--- app/controllers/account/boards_controller.rb
+++ app/controllers/account/boards_controller.rb
@@ -8,4 +8,27 @@ class Account::BoardsController < Account::BaseController
   def show
     @board = authorize(Board.find(params[:id]))
   end
+
+  def new
+    @board = authorize(Board.new)
+  end
+
+  def create
+    @board = authorize(Board.new(create_params))
+    @board.organization = current_user.organizations.last
+
+    if @board.save
+      respond_to do |format|
+        format.turbo_stream { render turbo_stream: turbo_stream.action(:redirect, account_board_path(@board)) }
+      end
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def create_params
+    params.require(:board).permit(:title)
+  end
 end
