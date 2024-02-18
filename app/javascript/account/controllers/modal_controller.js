62b7d534cfe9b8101a7af1f0843d5d9cb73f8fc2
Alexander Schrot <alexander@axs-labs.com>
2024-02-18 22:03:30 +0100

    add better modal handling via stimulus

--- app/javascript/account/controllers/modal_controller.js
+++ app/javascript/account/controllers/modal_controller.js
@@ -4,19 +4,48 @@ import eventManager from "/services/event_service"
 
 export default class extends Controller {
   connect() {
-    this.modal = new Modal(this.element)
+    this.element.addEventListener('turbo:frame-load', this.show.bind(this))
+    this.element.addEventListener('turbo:before-frame-render', this.close.bind(this))
+    this.unregisterCloseEvent = eventManager.onModalClose(this.close.bind(this))
 
-    this.modal.show()
-
-    this.unregisterCloseEvent = eventManager.onModalClose(this.hide.bind(this))
+    this.show()
   }
 
-  hide() {
-    this.modal.hide()
+  close() {
+    if (!this.getModalElement()) return
+    const modal = Modal.getInstance(this.getModalElement())
+
+    if (!modal) return
+
+    modal.hide()
+    modal.dispose()
+
+    this.element.src = null
+    this.element.replaceChildren()
+  }
+
+  show() {
+    if (!this.getModalElement()) return
+
+    const modal = Modal.getOrCreateInstance(this.getModalElement())
+    modal.show()
   }
 
   disconnect() {
     this.unregisterCloseEvent()
-    this.modal.dispose()
+    this.element.removeEventListener('turbo:frame-load', this.show.bind(this))
+    this.element.removeEventListener('turbo:before-frame-render', this.close.bind(this))
+
+    if (!this.getModalElement()) return
+    const modal = Modal.getInstance(this.getModalElement())
+
+    if (!modal) return
+
+    modal.hide()
+    modal.dispose()
+  }
+
+  getModalElement() {
+    return this.element.querySelector('.modal')
   }
 }
