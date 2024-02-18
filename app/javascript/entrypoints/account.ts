c224c98f6119f20245bbf7230117c42dddacefaf
Alexander Schrot <alexander@axs-labs.com>
2024-02-18 21:15:13 +0100

    add new turbo stream action to close modal

--- app/javascript/entrypoints/account.ts
+++ app/javascript/entrypoints/account.ts
@@ -2,6 +2,7 @@ import { Turbo } from "@hotwired/turbo-rails"
 import "@tabler/core"
 import { Application } from "@hotwired/stimulus"
 import { definitionsFromGlob } from 'stimulus-vite-helpers'
+import eventManager from "../services/event_service"
 
 const controllers = import.meta.glob('../account/controllers/**/*_controller.(js|ts)', { eager: true })
 
@@ -10,4 +11,7 @@ application.load(definitionsFromGlob(controllers))
 
 Turbo.StreamActions.redirect = function() {
   Turbo.visit(this.target);
+};
+
+Turbo.StreamActions.close_modal = function() {
 };
